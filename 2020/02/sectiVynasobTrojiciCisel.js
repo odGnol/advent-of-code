6cab080b1413049c3479286595a68f87d92e669f
gnolod <long.do@email.cz>
2021-11-28 23:09:46 +0100

    kanón na vrabce, aneb dokončení druhé části 1. dne

--- 2020/02/sectiVynasobTrojiciCisel.js
+++ 2020/02/sectiVynasobTrojiciCisel.js
@@ -2,5 +2,38 @@ import fs from "fs";
 
 let cilenySoucet = 2020;
 const buffer = fs.readFileSync("2020/01/dataset/vstupy.csv");
-console.log(buffer);
-console.log(cilenySoucet);
+
+const sectiVynasobTrojiciCisel = (ocekavanySoucet, buff) => {
+    let poleCisel = [];
+    let zpracovanaData = buff.toString().split('\n');
+
+    for (const cisloZDat of zpracovanaData) {
+        poleCisel.push(parseInt(cisloZDat));
+    }
+
+    const hledanaTrojiceCisel = [];
+
+    poleCisel.sort((a, b) => a - b);
+
+    for (let i = 0; i < poleCisel.length - 2; i++) {
+        let levyUkazatel = i + 1;
+        let pravyUkazatel = poleCisel.length - 1;
+
+        while (levyUkazatel < pravyUkazatel) {
+            let soucet = poleCisel[levyUkazatel] + poleCisel[pravyUkazatel] + poleCisel[i];
+            if (soucet === ocekavanySoucet) {
+                console.log(poleCisel[levyUkazatel] * poleCisel[pravyUkazatel] * poleCisel[i]);
+                hledanaTrojiceCisel.push([poleCisel[levyUkazatel], poleCisel[pravyUkazatel], poleCisel[i]]);
+                levyUkazatel++;
+                pravyUkazatel--;
+            } else if (soucet < ocekavanySoucet) {
+                levyUkazatel++;
+            } else if (soucet > ocekavanySoucet) {
+                pravyUkazatel--;
+            }
+        }
+    }
+    return hledanaTrojiceCisel;
+}
+
+sectiVynasobTrojiciCisel(cilenySoucet, buffer)
