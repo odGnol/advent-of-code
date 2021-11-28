ff3b066b1aba0e4097d700046c5e4f07b1f4cdc8
gnolod <long.do@email.cz>
2021-11-28 03:09:25 +0100

    fční prototyp

--- 2020/01/sectiVynasobCisla.js
+++ 2020/01/sectiVynasobCisla.js
@@ -1,19 +1,34 @@
 import fs from "fs";
 
-let zpracovanaData;
-let poleCisel = [];
+let cilenySoucet = 2020;
 const buffer = fs.readFileSync("2020/01/dataset/vstupy.csv");
 
-const zpracujCSV = (csv) => {
-    zpracovanaData = csv.toString().split('\n');
+const najdiSpravnouDvojiciCisel = (ocekavanySoucet, buff) => {
+    let poleCisel = [];
+    let zpracovanaData = buff.toString().split('\n');
 
-    for(const cisloZDat of zpracovanaData) {
+    for (const cisloZDat of zpracovanaData) {
         poleCisel.push(parseInt(cisloZDat));
+        poleCisel;
     }
 
-    return poleCisel;
+    let serazenaCisla = poleCisel.sort((a, b) => a - b);
+    let levyUkazatel = 0;
+    let pravyUkazatel = serazenaCisla.length - 1;
+
+    while (levyUkazatel < pravyUkazatel) {
+        let soucet = serazenaCisla[levyUkazatel] + serazenaCisla[pravyUkazatel];
+        console.log(soucet);
+        if (soucet === ocekavanySoucet) {
+            console.log("tady " + serazenaCisla[levyUkazatel] * serazenaCisla[pravyUkazatel]);
+            return serazenaCisla[levyUkazatel] * serazenaCisla[pravyUkazatel];
+        } else if (soucet < ocekavanySoucet) {
+            levyUkazatel++;
+        } else if (soucet > ocekavanySoucet) {
+            pravyUkazatel--;
+        }
+    }
+    return [];
 }
 
-zpracujCSV(buffer);
-
-console.log(poleCisel);
+najdiSpravnouDvojiciCisel(cilenySoucet, buffer);
