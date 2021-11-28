13ad88418785f1da4efe74bbd3b79c8588ae6ba9
gnolod <long.do@email.cz>
2021-11-28 02:24:33 +0100

    zpracování csv dat na pole čísel

---
+++ 2020/01/sectiVynasobCisla.js
@@ -0,0 +1,19 @@
+import fs from "fs";
+
+let zpracovanaData;
+let poleCisel = [];
+const buffer = fs.readFileSync("2020/01/dataset/inputs.csv");
+
+const zpracujCSV = (csv) => {
+    zpracovanaData = csv.toString().split('\n');
+
+    for(const cisloZDat of zpracovanaData) {
+        poleCisel.push(parseInt(cisloZDat));
+    }
+
+    return poleCisel;
+}
+
+zpracujCSV(buffer);
+
+console.log(poleCisel);
